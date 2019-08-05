809020eb093989f89602d82c9f61c740b2f6fc11
Tutinava <Tutinava@DESKTOP-BVB787E>
2019-08-05 21:14:27 +0200

    Add project files.

---
+++ CIS_Alias_generator/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CIS_Alias_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.lbl_alias = new System.Windows.Forms.Label();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(13, 46);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(35, 13);
+            this.lbl_name.TabIndex = 0;
+            this.lbl_name.Text = "Name";
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(83, 46);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(283, 20);
+            this.txt_input.TabIndex = 1;
+            // 
+            // txt_output
+            // 
+            this.txt_output.Location = new System.Drawing.Point(83, 96);
+            this.txt_output.Multiline = true;
+            this.txt_output.Name = "txt_output";
+            this.txt_output.Size = new System.Drawing.Size(440, 200);
+            this.txt_output.TabIndex = 2;
+            // 
+            // lbl_alias
+            // 
+            this.lbl_alias.AutoSize = true;
+            this.lbl_alias.Location = new System.Drawing.Point(16, 96);
+            this.lbl_alias.Name = "lbl_alias";
+            this.lbl_alias.Size = new System.Drawing.Size(29, 13);
+            this.lbl_alias.TabIndex = 3;
+            this.lbl_alias.Text = "Alias";
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Location = new System.Drawing.Point(448, 46);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(75, 23);
+            this.btn_generate.TabIndex = 4;
+            this.btn_generate.Text = "Generate";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 446);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.lbl_alias);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.txt_input);
+            this.Controls.Add(this.lbl_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.TextBox txt_output;
+        private System.Windows.Forms.Label lbl_alias;
+        private System.Windows.Forms.Button btn_generate;
+    }
+}
+
---
+++ CIS_Alias_generator/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CIS_Alias_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Generate button
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+            string input = txt_input.Text;
+            string[] separated = input.Split(null);
+            string[] nameSeparated = new string[separated.Length - 1];
+
+            string companyName;
+            string output = "";
+            string[] companyTypeGenerated = companyType(separated[0]);
+
+            string fullNativeAlias = "";
+            string shortNativeAlias = input;
+
+            for (int i = 1; i < separated.Length; i++)
+            {
+                nameSeparated[i - 1] = transliteration(separated[i]);
+                fullNativeAlias = fullNativeAlias + " " + separated[i];
+            }
+            companyName = fullNativeAlias;
+            fullNativeAlias = companyTypeGenerated[0] + fullNativeAlias;
+
+
+            string translitedShortNativeAlias = transliteration(shortNativeAlias);
+            string translitedFullNativeAlias = transliteration(fullNativeAlias);
+            string companyNameTranslited = transliteration(companyName);
+
+            output = shortNativeAlias + System.Environment.NewLine +
+                     fullNativeAlias + System.Environment.NewLine +
+                     translitedShortNativeAlias + System.Environment.NewLine +
+                     translitedFullNativeAlias + System.Environment.NewLine +
+                     companyTypeGenerated[1] + companyNameTranslited + System.Environment.NewLine +
+                     companyTypeGenerated[2] + companyNameTranslited + System.Environment.NewLine;
+
+            txt_output.Text = output.ToUpper();
+        }
+
+        private string[] companyType(string input)
+        {
+            string[] type = new string[3];
+
+            switch (input)
+            {
+                case "ОАО":
+                case "оао":
+                    type[0] = "ОТКРЫТОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО";
+                    type[1] = "OJSC";
+                    type[2] = "OPEN JOINT STOCK COMPANY";
+                    break;
+                case "ЗАО":
+                case "зао":
+                    type[0] = "ЗАКРЫТОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО";
+                    type[1] = "CJSC";
+                    type[2] = "CLOSED JOINT STOCK COMPANY";
+                    break;
+                case "ПАО":
+                case "пао":
+                    type[0] = "ПУБЛИЧНОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО";
+                    type[1] = "PJSC";
+                    type[2] = "PUBLIC JOINT STOCK COMPANY";
+                    break;
+                case "ЧАО":
+                case "чао":
+                    type[0] = "ЧАСТНОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО";
+                    type[1] = "PJSC";
+                    type[2] = "PRIVATE JOINT STOCK COMPANY";
+                    break;
+                case "АО":
+                case "ао":
+                    type[0] = "АКЦИОНЕРНОЕ ОБЩЕСТВО";
+                    type[1] = "JSC";
+                    type[2] = "JOINT STOCK COMPANY";
+                    break;
+                case "ООО":
+                case "ооо":
+                    type[0] = "ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ";
+                    type[1] = "LLC";
+                    type[2] = "LIMITED LIABILITY COMPANY";
+                    break;
+                case "МУП":
+                case "муп":
+                    type[0] = "МУНИЦИПАЛЬНОЕ УНИТАРНОЕ ПРЕДПРИЯТИЕ";
+                    type[1] = "MUE";
+                    type[2] = "MUNICIPAL UNITARY ENTERPRISE";
+                    break;
+                default:
+                    type[0] = input;
+                    type[1] = transliteration(input);
+                    type[2] = type[1];
+                    break;
+            }
+            return type;
+        } 
+
+        private string transliteration(string input)
+        {
+            bool previousVowel = true;
+            char current = ' ';
+            string english = "";
+            string result="";
+
+            char[] separated = input.ToCharArray();
+            for (int i = 0; i < separated.Length; i++)
+            {
+                current = separated[i];
+                switch (current)
+                {
+                    case 'А':
+                    case 'а':
+                        english = "A";
+                        previousVowel = true;
+                        break;
+                    case 'Б':
+                    case 'б':
+                        english = "B";
+                        previousVowel = false;
+                        break;
+                    case 'В':
+                    case 'в':
+                        english = "V";
+                        previousVowel = false;
+                        break;
+                    case 'Г':
+                    case 'г':
+                        english = "G";
+                        previousVowel = false;
+                        break;
+                    case 'Д':
+                    case 'д':
+                        english = "D";
+                        previousVowel = false;
+                        break;
+                    case 'Е':
+                    case 'е':
+                    case 'Ё':
+                    case 'ё':
+                        if (previousVowel)
+                        {
+                            english = "YE";
+                        }
+                        else
+                        {
+                            english = "E";
+                        } 
+                        previousVowel = true;
+                        break;
+                    case 'Ж':
+                    case 'ж':
+                        english = "ZH";
+                        previousVowel = false;
+                        break;
+                    case 'З':
+                    case 'з':
+                        english = "Z";
+                        previousVowel = false;
+                        break;
+                    case 'И':
+                    case 'и':
+                        english = "I";
+                        previousVowel = true;
+                        break;
+                    case 'Й':
+                    case 'й':
+                        previousVowel = true;
+                        english = "Y";
+                        break;
+                    case 'К':
+                    case 'к':
+                        english = "K";
+                        previousVowel = false;
+                        break;
+                    case 'Л':
+                    case 'л':
+                        english = "L";
+                        previousVowel = false;
+                        break;
+                    case 'М':
+                    case 'м':
+                        english = "M";
+                        previousVowel = false;
+                        break;
+                    case 'Н':
+                    case 'н':
+                        english = "N";
+                        previousVowel = false;
+                        break;
+                    case 'О':
+                    case 'о':
+                        english = "O";
+                        previousVowel = true;
+                        break;
+                    case 'П':
+                    case 'п':
+                        english = "P";
+                        previousVowel = false;
+                        break;
+                    case 'Р':
+                    case 'р':
+                        english = "R";
+                        previousVowel = false;
+                        break;
+                    case 'С':
+                    case 'с':
+                        english = "S";
+                        previousVowel = false;
+                        break;
+                    case 'Т':
+                    case 'т':
+                        english = "T";
+                        previousVowel = false;
+                        break;
+                    case 'У':
+                    case 'у':
+                        english = "U";
+                        previousVowel = true;
+                        break;
+                    case 'Ф':
+                    case 'ф':
+                        english = "F";
+                        previousVowel = false;
+                        break;
+                    case 'Х':
+                    case 'х':
+                        english = "KH";
+                        previousVowel = false;
+                        break;
+                    case 'Ц':
+                    case 'ц':
+                        english = "TS";
+                        previousVowel = false;
+                        break;
+                    case 'Ч':
+                    case 'ч':
+                        english = "CH";
+                        previousVowel = false;
+                        break;
+                    case 'Ш':
+                    case 'ш':
+                        english = "SH";
+                        previousVowel = false;
+                        break;
+                    case 'Щ':
+                    case 'щ':
+                        english = "SHCH";
+                        previousVowel = false;
+                        break;
+                    case 'Ъ':
+                    case 'ъ':
+                        english = "";
+                        previousVowel = true;
+                        break;
+                    case 'Ы':
+                    case 'ы':
+                        english = "Y";
+                        previousVowel = false;
+                        break;
+                    case 'Ь':
+                    case 'ь':
+                        english = "";
+                        previousVowel = true;
+                        break;
+                    case 'Э':
+                    case 'э':
+                        english = "E";
+                        previousVowel = true;
+                        break;
+                    case 'Ю':
+                    case 'ю':
+                        english = "YU";
+                        previousVowel = true;
+                        break;
+                    case 'Я':
+                    case 'я':
+                        previousVowel = true;
+                        english = "YA";
+                        break;
+                    case ' ':
+                        english = " ";
+                        previousVowel = true;
+                        break;
+                    default:
+                        english = "_";
+                        break;
+                }
+                result = result + english;
+            }
+
+            return result;
+        }
+    }
+}
