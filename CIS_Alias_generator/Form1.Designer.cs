e2b2509bd3cfda01833d70409f719067354c86fa
Tutinava <Tutinava@DESKTOP-BVB787E>
2019-08-05 21:24:37 +0200

    update

--- CIS_Alias_generator/Form1.Designer.cs
+++ CIS_Alias_generator/Form1.Designer.cs
@@ -82,7 +82,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 446);
+            this.ClientSize = new System.Drawing.Size(563, 446);
             this.Controls.Add(this.btn_generate);
             this.Controls.Add(this.lbl_alias);
             this.Controls.Add(this.txt_output);
